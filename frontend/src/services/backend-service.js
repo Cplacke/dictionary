e245b2f51ce9ebe80ffd381df1642f72f5aa9935
Placke <cplacke@LAMU02F71ZXMD6R.uhc.com>
2023-09-11 19:23:22 -0400

    hit deployed backend, not localhost

--- frontend/src/services/backend-service.js
+++ frontend/src/services/backend-service.js
@@ -1,6 +1,7 @@
+const BASE_URL = 'https://diction.deno.dev'
 
 export const searchDictionaryResults = async (term, learners = false) => {
-    const res = await fetch(`http://localhost:8000/search?term=${term}`, { 
+    const res = await fetch(`${BASE_URL}/search?term=${term}`, { 
         method: 'GET',
         headers: {
             'Access-Control-Allow-Origin': '*'
@@ -11,7 +12,7 @@ export const searchDictionaryResults = async (term, learners = false) => {
 }
 
 export const searchGiphy = async (term) => {
-    const res = await fetch(`http://localhost:8000/gif?term=${term}`, { 
+    const res = await fetch(`${BASE_URL}/gif?term=${term}`, { 
         method: 'GET',
         headers: {
             'Access-Control-Allow-Origin': '*'
