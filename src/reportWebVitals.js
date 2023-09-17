b2c3bdcc0da6c991ac01d099fe0bafd2b44f932c
Placke <cplacke@LAMU02F71ZXMD6R.uhc.com>
2023-09-17 11:04:48 -0400

    add publish build

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,3 +1,18 @@
+if (!String.prototype.replaceAll) {
+	String.prototype.replaceAll = function(str, newStr){
+
+		// If a regex pattern
+		if (Object.prototype.toString.call(str).toLowerCase() === '[object regexp]') {
+			return this.replace(str, newStr);
+		}
+
+		// If a string
+		return this.replace(new RegExp(str, 'g'), newStr);
+
+	};
+}
+
+
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
