ab817736f27a8d508fad31fe0fbf3876f1bb762d
cplacke <colin.m.plackee@optum.com>
2024-02-26 10:19:59 -0500

    ?term=X query param launch

--- src/routes/SearchPage.js
+++ src/routes/SearchPage.js
@@ -3,8 +3,24 @@ import {
     Footer,
     SearchResults,
 } from '../components'
+import { AppContext } from '../App'
+import { useEffect, useContext } from "react";
 
 export const SearchPage = () => { 
+
+    const {
+        searchTerm, setSearchTerm,
+    } = useContext(AppContext)
+
+    useEffect(() => {
+        const searchTerm = /term=(.*)/i.exec(
+            window.location.search
+        );
+        if (searchTerm[1]) {
+            setSearchTerm(searchTerm[1])
+        }
+    }, [])
+
     return (
         <div>
             <Navigation/>
